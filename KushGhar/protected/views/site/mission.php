0b980d1ec30fdc147c0308d26f9acfb353f11451
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-03-06 10:59:51 +0000

    Updated password alignment and services
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@29 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/site/mission.php
+++ KushGhar/protected/views/site/mission.php
@@ -2,15 +2,49 @@
 <div class="container">
     <div class="row-fluid">
         <div class="span12">
-            <div class="paddinground" style="height:375px">
+            <div class="paddinground"  >
                 <h2>Mission</h2>
                 <div class="static_page_heading"></div>
-                <div class="page_header_mag" style="padding-top:100px">
+                <div class="page_header_mag"  >
           	We at Kushghar want to make peoples homes and lives better by providing services to those with busy lives needing help at home and opportunities to those who want to take care of others. Together everyone has a happier home - KushGhar
                 </div>
-                <div class="static_page_heading" style="padding-bottom:100px"></div>
+                 
             </div>
             
+        </div>
+    </div>
+    <div class="row-fluid">
+        
+        <div class="span12">
+          <div class="paddinground">
+                     <div class="seven_sines ">
+                        <ul>
+                       	<li>
+                        	 Politics <span>without</span> principle.
+                        </li>
+                        <li>
+                        	Wealth <span>without</span> work.
+                        </li>
+                        <li>
+                        	Commerce <span>without</span> morality.
+                        </li>
+                        <li>
+                        	Pleasure <span>without</span> conscience.
+                        </li>
+                        <li>
+                        	Education <span>without</span> character.
+                        </li>
+                        <li>
+                        	Science <span>without</span> humility.
+                        </li>
+                        <li>
+                        	Worship <span>without</span> sacrifice.
+                        </li>
+
+                       </ul>
+                       </div>
+                </div>
+
         </div>
     </div>
 </div>

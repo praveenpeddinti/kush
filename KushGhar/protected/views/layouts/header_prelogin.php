8c65f4e454a169d8bd5c9fce5ed085c959e1582e
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-04-11 06:40:05 +0000

    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@106 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/layouts/header_prelogin.php
+++ KushGhar/protected/views/layouts/header_prelogin.php
@@ -14,36 +14,7 @@
                         <?php } ?>
                     </div>
                     
-<script src='//www.google.com/jsapi' type='text/javascript'></script>
-<!--<script type='text/javascript'>
-google.load('search', '1', {language: 'en', style: google.loader.themes.V2_DEFAULT});
-google.setOnLoadCallback(function() {
-  var customSearchOptions = {};
-  var orderByOptions = {};
-  orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
-  customSearchOptions['enableOrderBy'] = true;
-  customSearchOptions['orderByOptions'] = orderByOptions;
-  customSearchOptions['overlayResults'] = true;
-  var customSearchControl =   new google.search.CustomSearchControl('001516148304717743126:n3pykjuuf1e', customSearchOptions);
-  customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-  var options = new google.search.DrawOptions();
-  options.setAutoComplete(true);
-  customSearchControl.draw('cse', options);
-}, true);
-</script>-->
-<!--<script>
-  (function() {
-    var cx = '001516148304717743126:e8yk7xa3q54';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-</script>-->
-<gcse:search></gcse:search>
+
                     <!--header logo end -->
                     <!--h_right start -->
                     <div class="span9">

3bb1a726132fbffd93ec11f47f3bc81cec0dcbf4
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-04-28 12:20:43 +0000

    ie issues
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@185 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/layouts/header.php
+++ KushGhar/protected/views/layouts/header.php
@@ -18,6 +18,7 @@
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
             <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/html5.js" type="text/javascript"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/respond.js" type="text/javascript"></script>
            <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" rel="stylesheet">
         <![endif]-->
         <!-- Fav and touch icons -->

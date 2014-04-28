a9625beb0a1a473994431ce5e79ec46a934777d0
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-04-28 08:51:36 +0000

    profile uploaded
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@183 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/extensions/EAjaxUpload/qqFileUploader.php
+++ KushGhar/protected/extensions/EAjaxUpload/qqFileUploader.php
@@ -71,7 +71,7 @@ class qqFileUploader {
         $this->allowedExtensions = $allowedExtensions;
         $this->sizeLimit = $sizeLimit;
 
-        $this->checkServerSettings();
+        //$this->checkServerSettings();
 
         if (isset($_GET['qqfile'])) {error_log("enter File==================");
             $this->file = new qqUploadedFileXhr();

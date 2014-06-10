6d3c256482df59e15b9f6280228ae54f44053027
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-06-10 13:45:06 +0000

    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@220 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/user/serviceOrder.php
+++ KushGhar/protected/views/user/serviceOrder.php
@@ -1,12 +1,12 @@
      
             <div class="row-fluid">
                 <div class="span12">
-                    <h4 class="paddingL20">Order Place</h4> <hr>
+                    <h4 class="paddingL20">Order Conformation</h4> <hr>
                 </div>
             </div>            
             <div class="row-fluid">
                 <div class="span12">
-                    <h2 class="paddingL20"> Your Order is successsfully added. We Will contact you shortly.</h2> 
+                    <h2 class="paddingL20"> Your Order is successfully added. We Will contact you shortly.</h2> 
                     <h3 class="paddingL20">Order Number is : <?php echo $orderNumber;?></h3>
                 </div>
             </div>

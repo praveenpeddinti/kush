d67904da52e9c21cc8183daf1ec3e2326a2230b1
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-06-11 10:08:09 +0000

    mail functionality
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@228 0e8525e9-3983-421d-bfb9-d0e812aa3877

---
+++ KushGhar/protected/components/CommonUtility.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Developer Name: Suresh Reddy
+ * CommonUtility is the customized for common methods.
+ * All  common methods need to mention here.
+ */
+class CommonUtility 
+{
+    
+   /**
+ * Developer Name: Suresh Reddy & HariBabu
+ * this method is used for send a mail using below parameters.
+ * 
+    * 
+    * $view  basic template file
+    * $params array ,these array parames will bind with view file.
+    * $subject subject of email
+    * $toAddress toAddress , sender's email addresses
+    * $fromAddress fromAddress of User
+ */ 
+
+// this is local variable, which can accessiable in any function
+public function actionSendmail($view, $params, $subject, $toAddress, $fromAddress) {
+  error_log("enter Comm----------------");
+        try {
+            Yii::import('ext.yii-mail.YiiMailMessage');               
+            Yii::app()->mail->transportOptions = array(
+           'host' => 'webmail.kushghar.com',
+           'username' => 'no-reply@kushghar.com',
+           'password' => 'Kush1029',
+           'port' => 25,
+           'encryption' => 'ssl'
+           );
+            Yii::app()->mail->transportType = 'smtp';// Uncomment these when email is configured in admin section for Template management
+            $message = new YiiMailMessage;
+            $message->view = $view;
+            $message->setBody($params, 'text/html');
+            $message->subject = $subject;
+            $message->addTo($toAddress);
+            $message->from = $fromAddress;
+        if (Yii::app()->mail->send($message)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    catch (Exception $ex) {
+        error_log("Send Email Exception".$ex->getMessage());
+        }
+}
+
+     
+
+}

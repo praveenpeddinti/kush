e6435572e3928d25cf5aa67caa9bc642786428a3
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-06-16 11:47:53 +0000

    sent mail to KGTeam
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@241 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/extensions/mail/coActiveMailServer.php
+++ KushGhar/protected/extensions/mail/coActiveMailServer.php
@@ -37,6 +37,9 @@ class coActiveMailServer {
         }else if ($obj['mailType'] == "OrderPlaceToKGTeam") {
             //$this->getInvitationMail($obj, $host, $port, $authMail, $authPassword, $coActiveLogo,$date);
             $this->getOrderPlaceMailMessageToKGTeam($obj, $host, $port, $authMail, $authPassword, $coActiveLogo,$date);
+        }else if ($obj['mailType'] == "CustomerInvitationMailToKGTeam") {
+            //$this->getInvitationMail($obj, $host, $port, $authMail, $authPassword, $coActiveLogo,$date);
+            $this->getCustomerInvitationMailToKGTeam($obj, $host, $port, $authMail, $authPassword, $coActiveLogo,$date);
         }
 
 }
@@ -414,7 +417,67 @@ class coActiveMailServer {
         return $result;
     }
     
-    
+    function getCustomerInvitationMailToKGTeam($obj, $host, $port, $authMail, $authPassword,$coActiveLogo,$date) {
+        error_log($coActiveLogo."==&*****************&&&&&&&&&&&&&&&&********************in dependentMessage function=========");
+        
+        $template2 = '';
+
+        $temp2 = "/opt/lampp/htdocs/KushGhar/protected/extensions/mail/CustomerInvitationMailToKGTeam.html";
+        $handle2 = fopen($temp2, "r");
+        $data2 = file_get_contents($temp2);
+        $addAddress = $obj['toAddress'];
+        $subject = $obj['subject'];
+        $userName = $obj['userName'];
+        $message = $obj['message'];
+        $Kushmessage2 = "KushGhar - Making people's lives better, one home at a time.";
+        $employerName = $obj['employerName'];
+        $employerEmail = $obj['employerEmail'];
+        error_log($message."==&*******order*****1111111*****&&&&&&&&&&&&&&&&********************in dependentMessage function=========$addAddress===$userName");
+//        for ($i = 0; $i < sizeof($addAddress); $i++) {
+            if (trim($addAddress) != "") {
+                try {
+                    error_log("&*********praveen********&&&&&&&&&&&&&&&&********************in dependentMessage function=========$addAddress===$userName");
+                    $mail = new PHPMailer();
+                    $mail->IsSMTP();
+                    $mail->SMTPDebug = 0;
+                    $mail->Debugoutput = 'html';
+                    $mail->Host = $host;
+                    $mail->Port = $port;
+                    //$mail->SMTPSecure = 'ssl';
+                    $mail->SMTPSecure = '';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = $authMail;
+                    $mail->Password = $authPassword;
+                    $mail->SetFrom($employerEmail, $employerName);
+                    
+                    //$template2 = str_replace('{--EMAILIDS--}',stripslashes($emailIds),$template2);  
+                    $template2 = str_replace('{--USERNAME--}',stripslashes(ucwords("$userName")),$data2);                    
+                    $template2 = str_replace('{--DATE--}',stripslashes($date),$template2);
+                    $template2 = str_replace('{--MESSAGE--}',stripslashes($message),$template2);
+                    $template2 = str_replace('{--MESSAGE2--}',stripslashes($Kushmessage2),$template2);
+                    $template2 = str_replace('{--EMPLOYERNAME--}',stripslashes($employerName),$template2);
+                    $template2 = str_replace('{--COACTIVELOGO--}', stripslashes($coActiveLogo), $template2);
+                     $template2 = str_replace('{--SITEURL--}', YII::app()->params['SERVER_URL'], $template2);
+                     $template2 = str_replace('{--TOADDRESS--}',stripslashes($addAddress),$template2);
+                    $mail->AddAddress($addAddress, $userName);
+                    
+                    $mail->Subject =  $subject;
+                    $mail->Body = $template2;                    
+                    $mail->AltBody = 'This is a plain-text message body';
+                    if (!$mail->Send()) {
+                        $result = "failed";
+                        error_log("Mailer Error: " . $mail->ErrorInfo);
+                    } else {
+                        $result = "success";
+                        error_log("Message sent!");
+                    }
+                } catch (Exception $e) {
+                    error_log("@@@@@@@@@@@@@@@@@@@@@@@@Exception Occurred while mail sending in employee...@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2" . $e->getMessage());
+                }
+            }
+//        }
+        return $result;
+    }
     
 
 }

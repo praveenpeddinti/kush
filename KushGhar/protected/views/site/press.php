056ac4d0854937574a97e681ef9d7134cf3d7aee
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-06-24 09:33:39 +0000

    Added Predd Note
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@256 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/site/press.php
+++ KushGhar/protected/views/site/press.php
@@ -5,12 +5,25 @@
             <div class="paddinground" style="height:410px">
                 <h2>Press</h2>
                 <div class="static_page_heading"></div>
-                <div class="page_header_mag" style="padding-top:120px">
+                
+                
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="span2">
+                            <div class="aboutus_pic"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/kushghar_thumb.jpg" width="135" height="135"></div>
+                        </div>
+                        <div class="span10 paddingL20">
+                            
+                            <p style="padding-top:65px">NDTV published an article about KushGhar on 18<sup>th</sup> June 2014. Please <a href="http://gadgets.ndtv.com/internet/news/a-crowdfunding-project-to-make-domestic-chores-simple-and-help-the-underprivileged-543598" target="_blank"><i style="color:#6CA050">click here</i></a> to see the details.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="page_header_mag">
                     <p>KushGhar in the Press</p>
                     <p>Interested in writing about KushGhar? Email to <i style="color:#6CA050">press@kushghar.com</i></p>
                 </div>
-                
-            </div>
+                </div>
             
         </div>
     </div>

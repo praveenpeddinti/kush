b7ca2a22890c7e5e86b0f67045e7acb540ceb1a0
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-06-24 13:17:23 +0000

    Admin mail funtion
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@258 0e8525e9-3983-421d-bfb9-d0e812aa3877

---
+++ KushGhar/protected/views/mail/InvitationMail.php
@@ -0,0 +1,93 @@
+<html><body>
+
+<table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#262626" align="center">
+    <tbody><tr>
+            <td valign="top">
+                <!-- START OF EMAIL WRAPPER -->
+                <table width="650" cellspacing="0" cellpadding="0" border="0" bgcolor="#fcfcfc" align="center">
+                    <tbody><tr>
+                            <td>
+
+                                <!-- START OF Header Table -->
+                                <table width="650" cellspacing="0" cellpadding="0" border="0" bgcolor="#6CA050" >
+                                    <tbody>
+                                        <tr><td colspan='2'>&nbsp;</td></tr>
+                                        <tr>
+                                            <td valign="top" bgcolor="#fffff" align="left" >
+                                                
+                                                <a href="<?php echo YII::app()->params['SERVER_URL'];?>" target="_blank"><img alt="KushGhar" style="float:left" src="<?php echo $Logo;?>"/></a>
+                                                
+                                            </td>
+                                            <td bgcolor="#fffff" valign="center" ><h3>Welcome to KushGhar</h3><br/><b>Phone:</b> 1-800-3070-6959</td>
+                                        </tr>
+                                        <tr><td colspan='2'>&nbsp;</td></tr>
+                                    </tbody></table>
+                                <!-- END OF Header Table -->
+                                <!-- START OF full width Table --> 
+                                <table width="630" cellspacing="0" cellpadding="0" border="0" align="center">
+                                    <tbody>
+                                    <td width="630" valign="top" align="left" style="font-family: Helvetica, Arial, sans-serif; font-size: 12px; line-height: 18px;">
+                                       <p style="font-size:14px;line-height:1.7;color:#666;font-weight:bold">Hi, </p>  
+                                        <p style="font-size:14px;line-height:1.7;">
+                                            KushGhar has added service in your area and we are very pleased to welcome you to the KushGhar family.
+                                        </p>
+                                        <p style="font-size:14px;line-height:1.7;">
+                                            Please click on the following link and it will take you to our registration page.
+                                        </p>
+                                        <p style="font-size:14px;line-height:1.7;"><a href="<?php echo $Message;?>" target="_blank">Click here to get into Kushghar</a></p>
+                                        <p style="font-size:14px;line-height:1.7;color:#666;"> KushGhar - Making people's lives better, one home at a time.</p>
+
+                                        <span style="font-size:14px;line-height:1.7;color:#666;"> 
+                                            <ul>
+                                                <li>Service providers are background checked and certified</li>
+                                                <li>100% Satisfaction Guarantee</li>
+                                                <li>Service providers bring all supplies and equipment</li>
+                                                
+                                            </ul>
+                                        </span>
+                                        
+                                        <p style="font-size:16px;line-height:1.7;">
+                                            Thank you,<br/>
+                                            KushGhar.    
+                                        </p>
+
+                                    </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: Helvetica, sans-serif; font-size: 13px; color: #373737; line-height: 16px;">&nbsp;</td>
+                        </tr>
+                    </tbody></table>
+                <!-- END OF full width Table -->
+
+
+
+
+
+                <!-- START OF FOOTER TABLE -->
+                <table width="650" cellspacing="0" align="center" cellpadding="0" border="0" bgcolor="#fcfcfc">
+
+                    <tbody><tr>
+                            <td valign="top" bgcolor="#fcfcfc" align="left" height="26" colspan="4">&nbsp;</td>
+                        </tr>
+
+
+                        <tr>
+                            <td width="10" valign="top" bgcolor="#ededed" align="left" height="58">&nbsp;</td>
+                            <td  bgcolor="#ededed" align="left" style="font-family: Helvetica, sans-serif; font-size: 11px; color: #757887; line-height: 16px;">Copyright &copy; 2014 <a href="<?php echo YII::app()->params['SERVER_URL'];?>" target="_blank" style="text-decoration: none;"> KushGhar</a> All Rights Reserved.
+                            </td>
+
+                        </tr>
+
+                    </tbody></table>
+                <!-- END OF FOOTER TABLE -->
+
+
+
+
+            </td>
+        </tr>
+    </tbody></table><!-- END OF EMAIL WRAPPER -->
+    </body></html>
+
+
+

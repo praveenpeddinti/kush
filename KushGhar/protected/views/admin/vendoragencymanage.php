2b7ac2a1294e8217cc43b971f63a417cb2ec2627
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-07-11 05:44:48 +0000

    Invite vendor functionality
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@336 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/admin/vendoragencymanage.php
+++ KushGhar/protected/views/admin/vendoragencymanage.php
@@ -18,7 +18,7 @@
                 error_log("Status==============".$status);
                 echo $status; ?></td>
         <td>
-            <input id="usera_<?php echo $row['vid']; ?>" data-id="<?php echo $row['vid']; ?>" invite-status="<?php echo $row['status']; ?>" type="button" value=" " class="<? if ($row['status'] == '0') echo 'icon_active'; if ($row['status'] == '1') echo 'icon_inactive';?>" alt="Status" title="Change Status"/>
+            <input id="usera_<?php echo $row['vid']; ?>" data-id="<?php echo $row['vid']; ?>" invite-status="<?php echo $row['status']; ?>" type="button" value=" " class="<? if ($row['status'] == '0') echo 'icon_inactive'; if ($row['status'] == '1') echo 'icon_active';?>" alt="Status" title="Change Status"/>
         </td>
     </tr>
     <?php }

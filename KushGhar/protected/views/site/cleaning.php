e6810c96869580fcefd54487871e7f226ff067e2
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-07-24 13:01:48 +0000

    left menu tab changes
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@408 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/site/cleaning.php
+++ KushGhar/protected/views/site/cleaning.php
@@ -40,10 +40,10 @@
                         <div id="services" class="collapse in">
                             <div class="selected_tab">Services</div>
                             <ul class="l_menu_sub_menu">
-                                <li class="active"><a href="/site/cleaning"> House Cleaning</a></li>
-                                <li ><a href="/site/carwash"> Car Cleaning</a> </li>
-                                <li ><a href="/site/stewards">  Stewards / Stewardesses</a> </li>
-                                <li ><a href="/site/moreservices">  More ...</a> </li>
+                                <a href="/site/cleaning"><li class="active"> House Cleaning</li></a>
+                                <a href="/site/carwash"><li> Car Cleaning </li></a>
+                                <a href="/site/stewards"><li>  Stewards / Stewardesses </li></a>
+                                <a href="/site/moreservices"><li>  More ...</li></a>
                             </ul>
                         </div>
                     </div>

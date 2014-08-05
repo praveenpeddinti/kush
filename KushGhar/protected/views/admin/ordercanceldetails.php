971c1079b7ba938fac0799cbbed17490354fa388
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-08-05 04:43:07 +0000

    Admin side when the order close it shows popup
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@433 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/admin/ordercanceldetails.php
+++ KushGhar/protected/views/admin/ordercanceldetails.php
@@ -11,15 +11,13 @@
     <div class=" span6">
         <?php echo $form->label($model, '<abbr title="required">*</abbr> Total Service Hours'); ?>
         <?php echo $form->dropDownList($model,'TotalServiceHours', array(''=>'Select','1' => '1', '1.5' => '1.5','2' => '2', '2.5' => '2.5','3' => '3', '3.5' => '3.5','4' => '4', '4.5' => '4.5','5' => '5', '5.5' => '5.5','6' => '6', '6.5' => '6.5'
-            ,'7' => '7', '7.5' => '7.5','8' => '8', '8.5' => '8.5','9' => '9', '9.5' => '9.5','10' => '10', '10.5' => '10.5','11' => '11', '11.5' => '11.5','12' => '12', '12.5' => '12.5','13' => '13', '13.5' => '13.5','14' => '14', '14.5' => '14.5'
-            ,'15' => '15', '15.5' => '15.5','16' => '16', '16.5' => '16.5','17' => '17', '17.5' => '17.5','18' => '18', '18.5' => '18.5','19' => '19', '19.5' => '19.5','20' => '20', '20.5' => '20.5','21' => '21', '21.5' => '21.5','22' => '22', '22.5' => '22.5'
-            ,'23' => '23', '23.5' => '23.5','24' => '24', '24.5' => '24.5'), array('class' => 'span8'));?>
+            ,'7' => '7', '7.5' => '7.5','8' => '8', '8.5' => '8.5','9' => '9', '9.5' => '9.5','10' => '10', '10.5' => '10.5','11' => '11', '11.5' => '11.5','12' => '12'), array('class' => 'span8'));?>
         <?php echo $form->error($model,'TotalServiceHours'); ?>
         
     </div>
         <div class=" span6">
         <?php echo $form->label($model, '<abbr title="required">*</abbr> Total Service People'); ?>
-        <?php echo $form->textField($model,'TotalServicePeople',array('maxLength' => 4, 'class'=>'span4')); ?>
+        <?php echo $form->textField($model,'TotalServicePeople',array('maxLength' => 4, 'class'=>'span4','onkeypress'=>'return isNumberKey(event);')); ?>
         <?php echo $form->error($model,'TotalServicePeople'); ?>
         
     </div>
@@ -55,7 +53,7 @@
             $("#OrderForm_TotalServiceHours_em_").hide();
             $("#OrderForm_TotalServicePeople_em_").show();
             $("#OrderForm_TotalServicePeople_em_").addClass('errorMessage');
-            $("#OrderForm_TotalServicePeople_em_").text("Please Enter +ve Number");
+            $("#OrderForm_TotalServicePeople_em_").text("Please Enter number only");
                 return false;
         }
         else

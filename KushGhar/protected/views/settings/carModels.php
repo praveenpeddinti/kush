1f7aa0cb9ab94ce5bd1989ba83d1b20e8ea8d064
Sushmaja <Sushmaja@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-09-08 10:42:17 +0000

    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@559 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/settings/carModels.php
+++ KushGhar/protected/views/settings/carModels.php
@@ -18,6 +18,8 @@
                                 <li><a href="/admin/usermanagement"> <i class="fa fa-user"></i> User Management</a></li>
                                 <li><a href="/admin/vendormanagement"> <i class="fa fa-user"></i> Vendor Management</a></li>
                                 <li><a href="/admin/reviews"> <i class="fa fa-user"></i> Review/Feedback</a></li>
+                                <li><a href="/admin/invoice"> <i class="fa fa-list-alt"></i> Invoice Management</a></li>
+                                <li><a href="/admin/payments"> <i class="fa fa-file"></i> Payments</a></li>
                                 <li class="active"><a href="/settings/carMakes"> <i class="fa fa-cog"></i> Settings</a></li>
                             </ul>
                         </div>

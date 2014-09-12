be612f8653bc51f43ba22937ef15673a84908f67
supraja.arumalla <supraja.arumalla@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-09-12 07:19:29 +0000

    View of vendor proof in vendor management view details
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@580 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/admin/getfulldetails.php
+++ KushGhar/protected/views/admin/getfulldetails.php
@@ -49,18 +49,38 @@ $address;
         <td>
             <label>Identity Proof</label>
             <div class="thumbnail" style="width: 150px; height: 150px;margin-bottom:10px">
-                <img style="width:150px;height:150px" src="<?php echo isset ($userAllDetails['proof_image_file_location'])?$userAllDetails['proof_image_file_location']:'/images/profile/none.jpg';  ?>" /></div>
+                    <img style="width:150px;height:150px" src="<?php echo isset ($userAllDetails['proof_image_file_location'])?$userAllDetails['proof_image_file_location']:'/images/profile/none.jpg';  ?>" title="<?php echo isset ($userAllDetails['proof_image_file_location'])?$userAllDetails['proof_image_file_location']:'/images/profile/none.jpg';  ?>" onclick="popUpImageForCompound('<?php echo isset ($userAllDetails['proof_image_file_location'])?$userAllDetails['proof_image_file_location']:'/images/profile/none.jpg';  ?>')" /></div>
         </td>
         <td>
             <label>Address Proof</label>
             <div class="thumbnail" style="width: 150px; height: 150px;margin-bottom:10px">
-                <img style="width:150px;height:150px" src="<?php echo isset ($userAllDetails['address_image_file_location'])?$userAllDetails['address_image_file_location']:'/images/profile/none.jpg';  ?>" /></div>
+                <img style="width:150px;height:150px" src="<?php echo isset ($userAllDetails['address_image_file_location'])?$userAllDetails['address_image_file_location']:'/images/profile/none.jpg';  ?>" title="<?php echo isset ($userAllDetails['address_image_file_location'])?$userAllDetails['address_image_file_location']:'/images/profile/none.jpg';  ?>" onclick="popUpImageForCompound('<?php echo isset ($userAllDetails['address_image_file_location'])?$userAllDetails['address_image_file_location']:'/images/profile/none.jpg';  ?>')" /></div>
         </td>
         <td>
             <label>Clearance Proof</label>
             <div class="thumbnail" style="width: 150px; height: 150px;margin-bottom:10px">
-                <img style="width:150px;height:150px" src="<?php echo isset($userAllDetails['clearance_image_file_location'])?$userAllDetails['clearance_image_file_location']:'/images/profile/none.jpg';  ?>" /></div>
+                <img style="width:150px;height:150px" src="<?php echo isset($userAllDetails['clearance_image_file_location'])?$userAllDetails['clearance_image_file_location']:'/images/profile/none.jpg';  ?>" title="<?php echo isset ($userAllDetails['clearance_image_file_location'])?$userAllDetails['clearance_image_file_location']:'/images/profile/none.jpg';  ?>" onclick="popUpImageForCompound('<?php echo isset ($userAllDetails['clearance_image_file_location'])?$userAllDetails['clearance_image_file_location']:'/images/profile/none.jpg';  ?>')" /></div>
         </td>
     </tr>
     <?php }?>
     </table>
+<div id="overlay_content_for_ImagePopup" style="position: absolute; z-index: 999999; display: none; left: -230px; top: -85px;">
+    <div style="position: relative" class="overlay_maindiv" id="overlay_maindiv">
+        <div class="overlay_close" style="right:-20px;top:0px"><img src="../images/overlayclose.png" alt="Close" border="0" onclick="closeoverlay_for_ImageCompound()"></div>
+        <div class="popupcontentstyle">
+            <div class="search_div2">
+                <center><img id="overlayImage" src="" ></center>
+            </div>
+ 
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    function popUpImageForCompound(source){
+        document.getElementById('overlayImage').src=source;
+        document.getElementById('overlay_content_for_ImagePopup').style.display='block';
+    }
+    function closeoverlay_for_ImageCompound(){
+        document.getElementById('overlay_content_for_ImagePopup').style.display='none';
+    }
+</script> 

37b65fa41b8841589709b41805e278066ef9499c
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@0e8525e9-3983-421d-bfb9-d0e812aa3877>
2014-09-18 12:56:00 +0000

    show the previous order cast and updated order cost in edit the order page in admin side
    git-svn-id: https://subversion.assembla.com/svn/kushghar/trunk@594 0e8525e9-3983-421d-bfb9-d0e812aa3877

--- KushGhar/protected/views/admin/updateorderdetails.php
+++ KushGhar/protected/views/admin/updateorderdetails.php
@@ -18,27 +18,27 @@
             <?php $Rooms = array(); 
             for( $i = 0; $i <= 5; ++$i ) $Rooms[ $i ] = $i; ?>
             <?php echo $form->label($model, 'Kitchen(s)'); ?>
-            <?php echo $form->dropDownList($model,'Kitchens', array('1' => '1','2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '0'=>'None'), array('class' => 'span12','onchange' => 'javascript:onChangeKitchen(this.value);','options' => array($getServiceDetails['total_kitchens'] => array('selected' => 'selected'))));?>
+            <?php echo $form->dropDownList($model,'Kitchens', array('1' => '1','2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '0'=>'None'), array('class' => 'span12','onchange' => 'javascript:onChangeOrder(this.value);','options' => array($getServiceDetails['total_kitchens'] => array('selected' => 'selected'))));?>
             <div id="KitchenTooltip" class="Additional_S_price" style="display:none"></div>
         </div>
         <div class=" span2">
             <?php echo $form->label($model, 'Bathroom(s)'); ?>
-            <?php echo $form->dropDownList($model,'BathRooms', array('1' => '1','2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6','7' => '7', '8'=>'8', '9'=>'9', '10'=>'10', '0'=>'None'), array('class' => 'span12','onchange' => 'javascript:onChangeBathRoom(this.value);','options' => array($getServiceDetails['total_bathRooms'] => array('selected' => 'selected'))));?>
+            <?php echo $form->dropDownList($model,'BathRooms', array('1' => '1','2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6','7' => '7', '8'=>'8', '9'=>'9', '10'=>'10', '0'=>'None'), array('class' => 'span12','onchange' => 'javascript:onChangeOrder(this.value);','options' => array($getServiceDetails['total_bathRooms'] => array('selected' => 'selected'))));?>
             <div id="BathRoomTooltip" class="Additional_S_price" style="display:none"></div>
         </div>                                    
         <div class=" span2">
             <?php echo $form->label($model, 'Bedroom(s)'); ?>
-            <?php echo $form->dropDownList($model,'BedRooms', array('1' => '1', '2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6','7' => '7', '8'=>'8', '9'=>'9', '10'=>'10', '0'=>'None'), array('class' => 'span12','onchange' => 'javascript:onChangeBedRoom(this.value);','options' => array($getServiceDetails['total_bedRooms'] => array('selected' => 'selected'))));?>
+            <?php echo $form->dropDownList($model,'BedRooms', array('1' => '1', '2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6','7' => '7', '8'=>'8', '9'=>'9', '10'=>'10', '0'=>'None'), array('class' => 'span12','onchange' => 'javascript:onChangeOrder(this.value);','options' => array($getServiceDetails['total_bedRooms'] => array('selected' => 'selected'))));?>
             <div id="BedRoomTooltip" class="Additional_S_price" style="display:none"></div>
         </div>
         <div class=" span3">                                           
             <?php echo $form->label($model, 'Living room(s)'); ?>
-            <?php echo $form->dropDownList($model,'LivingRooms', array('1' => '1','2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '0'=>'None'), array('class' => 'span10','onchange' => 'javascript:onChangeLivingRoom(this.value);','options' => array($getServiceDetails['total_livingRooms'] => array('selected' => 'selected'))));?>
+            <?php echo $form->dropDownList($model,'LivingRooms', array('1' => '1','2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '0'=>'None'), array('class' => 'span10','onchange' => 'javascript:onChangeOrder(this.value);','options' => array($getServiceDetails['total_livingRooms'] => array('selected' => 'selected'))));?>
             <div id="LivingRoomTooltip" class="Additional_S_price" style="display:none"></div>
         </div>
         <div class=" span2">
             <?php echo $form->label($model, 'Other'); ?>
-            <?php echo $form->dropDownList($model,'OtherRooms', array('0'=>'None','1' => '1', '2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6','7' => '7', '8'=>'8', '9'=>'9', '10'=>'10'), array('class' => 'span12','onchange' => 'javascript:onChangeOther(this.value);','options' => array($getServiceDetails['other_rooms'] => array('selected' => 'selected'))));?>
+            <?php echo $form->dropDownList($model,'OtherRooms', array('0'=>'None','1' => '1', '2' => '2', '3'=>'3', '4'=>'4', '5'=>'5', '6'=>'6','7' => '7', '8'=>'8', '9'=>'9', '10'=>'10'), array('class' => 'span12','onchange' => 'javascript:onChangeOrder(this.value);','options' => array($getServiceDetails['other_rooms'] => array('selected' => 'selected'))));?>
             <div id="OtherTooltip" class="Additional_S_price" style="display:none"></div>
         </div>
         <div class="row-fluid">
@@ -65,37 +65,91 @@
          </div>
 
 <script type="text/javascript">
-    function orderUpdate(){
-        var UOrder='Yes';
-        
-        var queryString = $("#updateorderdetails-form").serialize();
+    function onChangeOrder(value){
+        //var r1 = Number(value);
         var r1 = Number($("#HouseCleaningForm_Kitchens").val());
         var r2 = Number($("#HouseCleaningForm_BathRooms").val());
         var r3 = Number($("#HouseCleaningForm_BedRooms").val());
         var r4 = Number($("#HouseCleaningForm_LivingRooms").val());
         var r5 = Number($("#HouseCleaningForm_OtherRooms").val());
+        
         var a1 = Number(<?php echo $getServiceDetails['window_grills'];?>);
         var a2 = Number(<?php echo $getServiceDetails['cupboard_cleaning'];?>);
         var a3 = Number(<?php echo $getServiceDetails['fridge_interior'];?>);
         var a4 = Number(<?php echo $getServiceDetails['microwave_oven_interior'];?>);
+        
+        //alert("---K---"+r1+"---Bath---"+r2+"---Bed---"+r3+"---Liv---"+r4+"---Other---"+r5);
+        var priceRoom1 = ''; 
+                //Number(r1+r2);
+        var priceRoom2 = '';
+                //Number(r3+r4+r5);
+        var totalRoomsPrice='';
+        if ((r1 == 1) && (r2 == 1) && (r3 == 1) && (r4 == 1)) {
+        priceRoom1 = Number((r1+r2) * 250);
+        priceRoom2 = Number((r3+r4+r5) * 125);
+        totalRoomsPrice = Number(priceRoom1 + priceRoom2);
+        
+    } else {
+        var LR = '';
+        var BedR = '';
+        var BathR = '';
+        var KR = '';
+        if (r4 > 1) {
+            LR = Number((r4 - 1) * 125);
+        }
+        if (r3 > 1) {
+            BedR = Number((r3 - 1) * 125);
+        }
+        if (r2 > 1) {
+            BathR = Number((r2 - 1) * 250);
+        }
+        if (r1 > 1) {
+            KR = Number((r1 - 1) * 250);
+        }
+                            
+        priceRoom1 = Number(LR + BedR);
+        priceRoom2 = Number(BathR + KR);
+        var otherRoomsCost = Number(r5*125);
+        totalRoomsPrice = Number(otherRoomsCost+priceRoom1 + priceRoom2 + 750);
+       
+    }
+    var priceAddServices = Number((a1 + a2 + a3 + a4) * 250);
+    totalRoomsPrice= Number(totalRoomsPrice+priceAddServices);
+    $("#updateCost").show();
+    $("#UTotalTd").text(totalRoomsPrice);
+    }
+    function orderUpdate(){
+        var queryString = $("#updateorderdetails-form").serialize();
+        /*var UOrder='Yes';
+        
+        
+        var r1 = Number($("#HouseCleaningForm_Kitchens").val());
+        var r2 = Number($("#HouseCleaningForm_BathRooms").val());
+        var r3 = Number($("#HouseCleaningForm_BedRooms").val());
+        var r4 = Number($("#HouseCleaningForm_LivingRooms").val());
+        var r5 = Number($("#HouseCleaningForm_OtherRooms").val());
+        var a1 = Number(<?php //echo $getServiceDetails['window_grills'];?>);
+        var a2 = Number(<?php //echo $getServiceDetails['cupboard_cleaning'];?>);
+        var a3 = Number(<?php //echo $getServiceDetails['fridge_interior'];?>);
+        var a4 = Number(<?php //echo $getServiceDetails['microwave_oven_interior'];?>);
         var priceOne = Number(r1+r2);
         var priceTwo = Number(r3+r4+r5);
         var priceThree = Number(a1+a2+a3+a4);
-        var total = Number(((priceThree+priceOne)*250)+(priceTwo*125));
+        var total = Number(((priceThree+priceOne)*250)+(priceTwo*125));*/
         if(orderValidate()){
-             $("#updateCost").show();
+             /*$("#updateCost").show();
              $("#UTotalTd").text(total);
              if(UOrder=="Yes"){
                 var r=confirm("Updated Order Total is Rs. "+total+". Are you sure want to continue");
             }else {
                 r=true;
             }
-         if(r==true){   
+         if(r==true){*/   
          ajaxRequest('/admin/updateorderstatus', queryString, updateorderHandler)
-        }else{
-          $("#updateCost").hide(); 
+        //}else{
+        //  $("#updateCost").hide(); 
           
-        }
+        //}
     }
     }
     function orderValidate(){
@@ -123,7 +177,7 @@
             setTimeout(function() {
                 $('#myModalUpdateOrder').modal('hide');
             }, 3000);
-                
+             window.location.href = '<?php echo Yii::app()->request->baseUrl; ?>/admin/order';    
             
         }
     } 
